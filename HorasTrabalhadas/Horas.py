a6e1f7b9f7bac3c33cd431e422613fa56b493918
EyshilaIvanha <eyshilaivanha@gmail.com>
2026-04-08 16:09:38 -0300

    Add files via upload

---
+++ HorasTrabalhadas/Horas.py
@@ -0,0 +1,19 @@
+# 1. Definição das entradas (Você pode mudar os valores ou usar input())
+valor_hora = float(input("Quanto você ganha por hora? "))
+horas = float(input("Quantas horas você trabalhou no mês? "))
+
+# 2. Cálculos
+salario_bruto = valor_hora * horas
+ir = salario_bruto * 0.11
+inss = salario_bruto * 0.08
+sindicato = salario_bruto * 0.05
+salario_liquido = salario_bruto - ir - inss - sindicato
+
+# 3. Exibição dos resultados
+print("\n" + "="*20)
+print(f"+ Salário Bruto : R$ {salario_bruto:.2f}")
+print(f"- IR (11%) : R$ {ir:.2f}")
+print(f"- INSS (8%) : R$ {inss:.2f}")
+print(f"- Sindicato (5%) : R$ {sindicato:.2f}")
+print(f"= Salário Líquido : R$ {salario_liquido:.2f}")
+print("="*20)
